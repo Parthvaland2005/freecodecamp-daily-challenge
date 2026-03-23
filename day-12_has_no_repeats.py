5fa7b4511a173b4e1e265f63faa1cdcf495b172e
Parth Valand <parthvaland2005@gmail.com>
2026-03-23 13:38:40 +0530

    Added No Consecutive Repeats problem solution (Day 12)

---
+++ day-12_has_no_repeats.py
@@ -0,0 +1,11 @@
+# question: Given a string, determine if it has no repeating characters.
+# A string has no repeats if it does not have the same character two or more times in a row.
+
+def has_no_repeats(s):
+    for i in range(len(s)-1):
+        if s[i]==s[i+1]:
+            return False
+    return True
+
+print(has_no_repeats("hello"))
+print(has_no_repeats("hi world"))
