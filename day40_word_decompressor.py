3b02803cdf418b1b4f12d166ccc022803bf94cb3
Parth Valand <parthvaland2005@gmail.com>
2026-04-25 21:31:43 +0530

    day-40 word decompressor

---
+++ day40_word_decompressor.py
@@ -0,0 +1,48 @@
+def decompress(s):
+    
+    tokens = s.split()
+    result = []
+    
+    for token in tokens:
+        if token.isdigit():
+            index = int(token) - 1   
+            result.append(result[index])
+        else:
+            result.append(token)
+    
+    return " ".join(result)
+
+def test_case(input_data, expected):
+    result = decompress(input_data)
+    if result == expected:
+        print("PASS")
+    else:
+        print("FAIL")
+        print("Result  :", result)
+        print("Expected:", expected)
+    print("-" * 50)
+
+test_case(
+    "practice makes perfect and 3 1 2 3",
+    "practice makes perfect and perfect practice makes perfect"
+)
+
+test_case(
+    "hello 1 1",
+    "hello hello hello"
+)
+
+test_case(
+    "the cat sat on 1 mat 4 which 1 2 3",
+    "the cat sat on the mat on which the cat sat"
+)
+
+test_case(
+    "the more you know 1 2 3 realize 3 don't 4",
+    "the more you know the more you realize you don't know"
+)
+
+test_case(
+    "lorem ipsum dolor sit per elit donec 4 nostra libero 5 7 ligula 4 gravida at 6 vitae a 6 sodales 7 en 7 16 3 nam 13 dignissim risus 16 13 5 27 2 2 15 23 6 5 2 13 23 15 5 21 4 16 27 1 4 5 10 23 2 6 4 21 4 30 6 30 2 6 16 15 18 23 29 27 4 18 sollicitudin 5 9 5 4 10",
+    "lorem ipsum dolor sit per elit donec sit nostra libero per donec ligula sit gravida at elit vitae a elit sodales donec en donec at dolor nam ligula dignissim risus at ligula per nam ipsum ipsum gravida en elit per ipsum ligula en gravida per sodales sit at nam lorem sit per libero en ipsum elit sit sodales sit risus elit risus ipsum elit at gravida vitae en dignissim nam sit vitae sollicitudin per nostra per sit libero"
+)
